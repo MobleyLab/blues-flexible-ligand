697cc6bd94dd8df06d75ca9178b10dbb3a2a90df
Sukanya Sasmal <sukanyasasmal@gmail.com>
2019-09-25 16:15:16 -0700

    ligand1

---
+++ ligand1/MD/run_MD.sh
@@ -0,0 +1,11 @@
+# Usage: bash run_MD.sh
+
+module load cuda/8.0.44
+
+mkdir rst PBS netcdf mdout mdinfo
+
+# running 100 ns MD on OpenMM
+python3 ../../scripts/mdin/md.py ../ligand1.equi.rst ../complex_wat.prmtop > PBS/md.log
+
+# post-processing blues MD trajectory for torsion distribution
+cpptraj -i dihedral.ptraj
